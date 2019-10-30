26f499e00901b630ba4a4ef417c23fb966beef82
Rutger Bresjer <rutger@woost.tech>
2019-10-30 11:16:08 +0100

    Add Prismic routes to web-middleware

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,9 @@
 <?php
 $types = config('laravel-prismic.types');
 
-foreach ($types as $type) {
-    Route::get($type::getRoute(), 'Woost\LaravelPrismic\Controller@show')
-        ->name(config('laravel-prismic.route_prefix') . '.' . $type::getTypeName() . '.show');
-}
+Route::middleware('web')->group(function () use ($types) {
+    foreach ($types as $type) {
+        Route::get($type::getRoute(), 'Woost\LaravelPrismic\Controller@show')
+            ->name(config('laravel-prismic.route_prefix') . '.' . $type::getTypeName() . '.show');
+    }
+});
